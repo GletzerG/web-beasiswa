917f4ea24eda2cf75f3df88cde553616323c7b87
phasya <adityaabyp@gmail.com>
2025-07-25 14:06:41 +0800

    navbar update

--- resources/views/back/layout/navbar.blade.php
+++ resources/views/back/layout/navbar.blade.php
@@ -24,7 +24,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="http://127.0.0.1:8000/">Coba-coba</a>
+        <a class="navbar-brand" href="http://127.0.0.1:8000/">Pendaftaran Beasiswa</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
             aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu

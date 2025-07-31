db857a22473e9400419e1e23246736c3c6e72992
phasya <adityaabyp@gmail.com>
2025-07-31 08:38:59 +0800

    hapus login

--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -4,14 +4,14 @@
     <div class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
         <div>
             <div class="d-grid gap-3">
-                <a href="{{ route('pendaftaran.daerah') }}"
+                <a href="https://forms.gle/V4euvswe2in3QMdj8"
                     class="btn btn-outline-primary btn-lg rounded-pill shadow">Pendaftaran Beasiswa Daerah Operasional</a>
-                <a href="{{ route('pendaftaran.akademik') }}"
+                <a href="https://forms.gle/V4euvswe2in3QMdj8"
                     class="btn btn-outline-success btn-lg rounded-pill shadow">Pendaftaran Beasiswa Prestasi Akademik</a>
-                <a href="{{ route('pendaftaran.nonakademik') }}"
+                <a href="https://forms.gle/V4euvswe2in3QMdj8"
                     class="btn btn-outline-warning btn-lg rounded-pill shadow">Pendaftaran Beasiswa Prestasi Non
                     Akademik</a>
-                <a href="{{ route('pendaftaran.kurangmampu') }}"
+                <a href="https://forms.gle/V4euvswe2in3QMdj8"
                     class="btn btn-outline-danger btn-lg rounded-pill shadow">Pendaftaran Beasiswa Kurang Mampu</a>
             </div>
         </div>
